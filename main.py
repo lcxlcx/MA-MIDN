c6f27e670b843ec0048dad7e922d6c0db7cd1585
lcxlcx <33259639+lcxlcx@users.noreply.github.com>
2021-04-26 16:40:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from config.multiple_instance_config import get_config
 from dataloader.bag_loader import get_test_loader, get_train_loader
 from task.train_Ind import Trainer_Ind
 
-from OICR.task.train import Trainer_MIL as train
 config, unparsed = get_config()
 
 
@@ -12,12 +11,9 @@ test_dataset = get_test_loader(config.testdata_dir,config.batch_size,config.num_
 
 data_loader = (train_dataset, test_dataset)
 
-# trainer = Trainer_MIL_Softmax(config, data_loader)
-# trainer = train(config, data_loader)
+
 
 trainer = Trainer_Ind(config, data_loader)
-# trainer = Trainer_MIL_Resnet(config, data_loader)
-# trainer = Trainer_DMIL(config, data_loader)
-# trainer = Trainer_MIL_SeResnet(config, data_loader)
-# trainer = Trainer_out1_DMIL(config, data_loader)
+# trainer = Trainer_DML(config, data_loader)
+
 trainer.train()
